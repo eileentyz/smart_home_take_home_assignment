cc970ffad021911b32be10f70d98d93595d9d531
Eileen Teoh Yan Zhen <eileenteoh10399@gmail.com>
2026-05-13 17:00:16 +0800

    Updated

--- build/smart_lighting_controller/build/lib/smart_lighting_controller/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Smart lighting controller package for ROS 2."""
-
-from smart_lighting_controller.lighting_controller import LightingController
-
-__all__ = ['LightingController']
--- build/smart_lighting_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/eileen/Documents/GitHub/smart_home_take_home_assignment/install/smart_lighting_controller'
